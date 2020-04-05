251eb341bcbb94e64a77b4ce761f06b8cb00451e
gvajd <gvajd@Vajdi-HP>
2020-04-05 19:59:39 +0200

    3. feladat

--- Telefonkonyvecske/src/telefonkonyvecske/Telefonkonyvecske.java
+++ Telefonkonyvecske/src/telefonkonyvecske/Telefonkonyvecske.java
@@ -6,6 +6,7 @@
 package telefonkonyvecske;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -24,13 +25,23 @@ public class Telefonkonyvecske {
         telefonkonyv.put(202274907,"Vajda Gergely");
         telefonkonyv.put(209706789,"Vajda Tamás");
         telefonkonyv.put(203944931,"Vajda János");
-        
+        telefonkonyv.put(205876954,"Böröcz Gergely");
         System.out.println("1. feladat: "+telefonkonyv);
+        
         //2. Tudjon keresni nevet telefonszám alapján (megmondja, ha van, hogy kié, illetve visszajelzi, ha nincs).
         System.out.println("2. feladat: "+telefonkonyv.get(202274907));
+        
         //3. Tudjon keresni név alapján (adja vissza azoknak a számoknak a listáját, akik például Jancsi nevű emberekhez tartoznak).
+        Iterator it= telefonkonyv.entrySet().iterator();
+        Map.Entry kereses= (Map.Entry) it.next();
+        if (kereses.getValue().equals("Vajda János")) {
+            System.out.println("3. feladat: "+kereses.getKey());
+        }
+        
         //4. Kérdezzük le az összes számot.
+        
         //5. Kérdezzük le az összes ember nevét. 
+        
         //6. Tudjon töröln telefonszám alapján.
         
     }
