84823644a0036bcd6e92a059b6c0614c4271c610
gvajd <gvajd@Vajdi-HP>
2020-04-05 19:25:50 +0200

    2. feladat

--- Telefonkonyvecske/src/telefonkonyvecske/Telefonkonyvecske.java
+++ Telefonkonyvecske/src/telefonkonyvecske/Telefonkonyvecske.java
@@ -25,7 +25,9 @@ public class Telefonkonyvecske {
         telefonkonyv.put(209706789,"Vajda Tamás");
         telefonkonyv.put(203944931,"Vajda János");
         
+        System.out.println("1. feladat: "+telefonkonyv);
         //2. Tudjon keresni nevet telefonszám alapján (megmondja, ha van, hogy kié, illetve visszajelzi, ha nincs).
+        System.out.println("2. feladat: "+telefonkonyv.get(202274907));
         //3. Tudjon keresni név alapján (adja vissza azoknak a számoknak a listáját, akik például Jancsi nevű emberekhez tartoznak).
         //4. Kérdezzük le az összes számot.
         //5. Kérdezzük le az összes ember nevét. 
