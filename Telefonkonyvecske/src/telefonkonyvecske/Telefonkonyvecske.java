6af35d9af7a87b9d13b467ea0f02e1165dc29b85
gvajd <gvajd@Vajdi-HP>
2020-04-06 17:25:58 +0200

    3. is jó

--- Telefonkonyvecske/src/telefonkonyvecske/Telefonkonyvecske.java
+++ Telefonkonyvecske/src/telefonkonyvecske/Telefonkonyvecske.java
@@ -20,45 +20,46 @@ public class Telefonkonyvecske {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         //1. Legyen képes belerakni a telefonkönyvbe egy telszám-név párt.
-        Map<Integer,String> telefonkonyv= new HashMap<>();
-        telefonkonyv.put(202274907,"Vajda Gergely");
-        telefonkonyv.put(209706789,"Vajda Tamás");
-        telefonkonyv.put(203944931,"Vajda János");
-        telefonkonyv.put(205876954,"Böröcz Gergely");
-        System.out.println("1. feladat: "+telefonkonyv);
-        
+        Map<Integer, String> telefonkonyv = new HashMap<>();
+        telefonkonyv.put(202274907, "Vajda Gergely");
+        telefonkonyv.put(209706789, "Vajda Tamás");
+        telefonkonyv.put(203944931, "Vajda János");
+        telefonkonyv.put(205876954, "Böröcz Gergely");
+        System.out.println("1. feladat: " + telefonkonyv);
+
         //2. Tudjon keresni nevet telefonszám alapján (megmondja, ha van, hogy kié, illetve visszajelzi, ha nincs).
-        Scanner sc= new Scanner(System.in, "windows-1252");
+        Scanner sc1 = new Scanner(System.in);
         System.out.println("2. feladat: Kérem adja meg a keresendő telefonszámot (pl. 204587596)!");
-        Integer telkeres=sc.nextInt();
-        if (telefonkonyv.get(telkeres)==null) {
+        Integer telkeres = sc1.nextInt();
+        if (telefonkonyv.get(telkeres) == null) {
             System.out.println("Nem található ilyen telefonszám a névjegyzékben.");
-        }else{
-        System.out.println("A keresett telefonszámhoz tartozó személy: "+telefonkonyv.get(telkeres));
+        } else {
+            System.out.println("A keresett telefonszámhoz tartozó személy: " + telefonkonyv.get(telkeres));
         }
         //3. Tudjon keresni név alapján (adja vissza azoknak a számoknak a listáját, akik például Jancsi nevű emberekhez tartoznak).
         System.out.println("3. feladat: Kérem adja meg a keresett személy nevét!");
-        String nevkeres=sc.next();
-        System.out.println(nevkeres);
-        Iterator it= telefonkonyv.entrySet().iterator();
-        while(it.hasNext()){
-        Map.Entry kereses= (Map.Entry) it.next();
+        Scanner sc2 = new Scanner(System.in, "windows-1252");
+        String nevkeres = sc2.nextLine();
+        Iterator it = telefonkonyv.entrySet().iterator();
+        Integer jartItt=0;
+        while (it.hasNext()) {
+            Map.Entry kereses = (Map.Entry) it.next();
             if (kereses.getValue().equals(nevkeres)) {
-            System.out.println(kereses.getKey());
-            }else{
-                System.out.println("Nem található ilyen név a névjegyzékben.");
-            }
+                System.out.println(kereses.getKey());
+                jartItt=1;
+            } 
 
+        }
+        if (jartItt==0) {
+            System.out.println("Nem található ilyen név a névjegyzékben.");
         }
         //4. Kérdezzük le az összes számot.
         System.out.println("4. feladat: ");
-        
+
         //5. Kérdezzük le az összes ember nevét. 
-        
         //6. Tudjon töröln telefonszám alapján.
-        
     }
-    
+
 }
