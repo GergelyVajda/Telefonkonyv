1b60f484b4797f59e27c275d1b125718e00a7da0
gvajd <gvajd@Vajdi-HP>
2020-04-06 17:56:32 +0200

    Minden kész

--- Telefonkonyvecske/src/telefonkonyvecske/Telefonkonyvecske.java
+++ Telefonkonyvecske/src/telefonkonyvecske/Telefonkonyvecske.java
@@ -42,10 +42,10 @@ public class Telefonkonyvecske {
         System.out.println("3. feladat: Kérem adja meg a keresett személy nevét!");
         Scanner sc2 = new Scanner(System.in, "windows-1252");
         String nevkeres = sc2.nextLine();
-        Iterator it = telefonkonyv.entrySet().iterator();
+        Iterator it1 = telefonkonyv.entrySet().iterator();
         Integer jartItt=0;
-        while (it.hasNext()) {
-            Map.Entry kereses = (Map.Entry) it.next();
+        while (it1.hasNext()) {
+            Map.Entry kereses = (Map.Entry) it1.next();
             if (kereses.getValue().equals(nevkeres)) {
                 System.out.println(kereses.getKey());
                 jartItt=1;
@@ -57,9 +57,41 @@ public class Telefonkonyvecske {
         }
         //4. Kérdezzük le az összes számot.
         System.out.println("4. feladat: ");
+        Iterator it2 = telefonkonyv.entrySet().iterator();
+        while (it2.hasNext()) {
+            Map.Entry kereses = (Map.Entry) it2.next();
+            System.out.println(kereses.getKey());
+
+        }
 
         //5. Kérdezzük le az összes ember nevét. 
-        //6. Tudjon töröln telefonszám alapján.
+        System.out.println("5. feladat: ");
+        Iterator it3 = telefonkonyv.entrySet().iterator();
+        while (it3.hasNext()) {
+            Map.Entry kereses = (Map.Entry) it3.next();
+            System.out.println(kereses.getValue());
+
+        } 
+        
+        //6. Tudjon törölni telefonszám alapján.
+        System.out.println("6. feladat: Kérem adja meg a törölni kívánt telefonszámot!");
+        telkeres=sc1.nextInt();
+        Iterator it4 = telefonkonyv.entrySet().iterator();
+        jartItt=0;
+        while (it4.hasNext()) {
+            Map.Entry kereses = (Map.Entry) it4.next();
+            if (kereses.getKey().equals(telkeres)) {
+                it4.remove();
+                System.out.println("A szám törlése sikeres!");
+                jartItt=1;
+            } 
+
+        }
+        if (jartItt==0) {
+            System.out.println("Nem található ilyen szám a névjegyzékben.");
+        }
+        System.out.println(telefonkonyv);
+        
     }
 
 }
